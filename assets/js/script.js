51d34e1a4827774e46b4c994494c320b43a391e8
KDeepika-brs <109024904+KDeepika-brs@users.noreply.github.com>
2023-12-13 18:11:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,185 +1,188 @@
-@import url("https://fonts.googleapis.com/css?family=Roboto");
+/** 
+Author:    Build Rise Shine with Nyros (BRS) 
+Created:   2023 
+Library / Component: Script file
+Description: Rock Paper Scissor game logic
+(c) Copyright by BRS with Nyros. 
+**/
 
-:root {
-  --primary-color: #003699;
-  --dark-color: #333333;
-  --light-color: #f4f4f4;
-  --lose-color: #dc3545;
-  --win-color: #28a745;
-  --modal-duration: 1s;
-}
+// Init player names
+let playerName;
+let playerNameInput = document.getElementById("playerOne");
+let gameBoard = document.getElementById("rock-paper-scissor");
+let startGameBtn = document.getElementById("startGame");
 
-* {
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
-}
+const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const restart = document.getElementById("restart");
+const modal = document.querySelector(".modal");
 
-body {
-  font-family: "Roboto", sans-serif;
-  line-height: 1.6;
-  
-  color: #333;
-}
+const scoreboard = {
+  player: 0,
+  computer: 0,
+};
 
-.form-group{
-  display: flex;
-  justify-content: center;
-}
+// Default theme
+let chathams_blue = "#1A4B84";
 
-.form-item {
-  margin: 20px;
-}
-
-
-
-.btn-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.start-btn,
-.restart-btn {
-  border: 2px solid black;
-  background-color: #ffc107;
-  padding: 6px;
-  font-weight: 600;
-}
-
-.container {
-  max-width: 1100px;
-  margin: auto;
-  overflow: hidden;
-  padding: 0 2rem;
-  text-align: center;
-  height: 80vh;
-  
-}
-
-.header {
-  text-align: center;
-  margin: 1rem 0;
-}
-
-.header h2 {
-  margin-bottom: 1rem;
-  margin-top: 1rem;
-  font-family: monospace;
-}
-
-.score {
-  font-size: 1.2rem;
-  font-family: monospace;
-  color: black;
-}
-
-.score p {
-  font-size: 20px;
-}
-
-.choices {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 2rem;
-  margin-top: 3rem;
-  text-align: center;
-}
-
-.choice {
-  cursor: pointer;
-}
-
-.choice:hover {
-  color: var(--primary-color);
-}
-
-.game-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-}
-
-.scoreBoardL {
-  border: 2px solid #000;
-  border-radius: 5px;
-}
-
-.modal-content img {
-  margin: 0 auto;
-}
-
-.game-img {
-  width: 150px;
-
-  margin-bottom: 20px;
-}
-
-.text-win {
-  color: var(--win-color);
-}
-
-.text-lose {
-  color: var(--lose-color);
-}
-
-.modal {
-  display: none;
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  overflow: auto;
-  background: rgba(0, 0, 0, 0.5);
-}
-
-.modal-content {
-  background: #fff;
-  text-align: center;
-  margin: 10% auto;
-  width: 350px;
-  border-radius: 10px;
-  padding: 3rem;
-  box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 7px 20px 0 rgba(0, 0, 0, 0.17);
-  animation-name: modalopen;
-  animation-duration: var(--modal-duration);
-}
-
-.modal-content h1 {
-  margin-bottom: 1rem;
-}
-
-.modal-content p {
-  font-size: 1.2rem;
-  margin-top: 1rem;
-}
-
-@keyframes modalopen {
-  from {
-    opacity: 0;
+// check player names and show the game board if true
+const gameStartListener = () => {
+  if (playerNameInput.value === "") {
+    alert("Please enter player name");
+    playerNameInput.focus();
+    return;
   }
+  playerName =
+    playerNameInput.value.charAt(0).toUpperCase() +
+    playerNameInput.value.slice(1);
+  gameBoard.style.display = "flex";
+  // Show score
+  score.innerHTML = `
+   <div class="row">
+     <div class="col-12 d-flex align-items-center mb-4">
+      <div class="col-6 text-start">${playerName}</div><div class="scoreBoardL p-2"> ${scoreboard.player}</div>
+     </div>
+     <div class="col-12 d-flex align-items-center">
+      <div class="col-6 text-start">Computer </div><div class="scoreBoardL p-2"> ${scoreboard.computer}</div>
+     </div>
+   </div>
+   `;
+  playerNameInput.style.display = "none";
+  startGameBtn.style.display = "none";
+};
 
-  to {
-    opacity: 1;
+// Init the event listener
+const gameStartButton = startGameBtn.addEventListener(
+  "click",
+  gameStartListener
+);
+
+// Init Game
+function initGame() {
+  gameBoard.style.display = "none";
+}
+
+initGame();
+
+// Play game
+function playGame(e) {
+  restart.style.display = "inline-block";
+  const playerChoice = e.target.id; // rock, paper, scissor
+  console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
+}
+
+// Get computers choice
+function getComputerChoice() {
+  const randomNumber = Math.random();
+  if (randomNumber < 0.34) {
+    return "rock";
+  } else if (randomNumber <= 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
   }
 }
 
-@media (max-width: 700px) {
-  .choice {
-    font-size: 110px;
+// Get game winner
+function getWinner(player, computer) {
+  if (player === computer) {
+    return "draw";
+  } else if (player === "rock") {
+    if (computer === "paper") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if (player === "paper") {
+    if (computer === "scissors") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if (player === "scissors") {
+    if (computer === "rock") {
+      return "computer";
+    } else {
+      return "player";
+    }
   }
 }
 
-@media (max-width: 500px) {
-  .choice {
-    font-size: 80px;
+// decide winner
+function showWinner(winner, computerChoice) {
+  console.log(computerChoice);
+  if (winner === "player") {
+    // Inc player score
+    scoreboard.player++;
+    // Show modal result
+    result.innerHTML = `
+      <h1 class="text-win">You Win</h1>
+      <img id="rock" class="choice" src="./assets/img/${computerChoice}.jpg" alt="rock"  width="150">
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</strong></p>
+    `;
+  } else if (winner === "computer") {
+    // Inc computer score
+    scoreboard.computer++;
+    // Show modal result
+    result.innerHTML = `
+      <h1 class="text-lose">You Lose</h1>
+      <img  class="choice" src="./assets/img/${computerChoice}.jpg" alt="rock" width="150">
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</strong></p>
+    `;
+  } else {
+    result.innerHTML = `
+      <h1>It's A Draw</h1>
+      <img  class="choice" src="./assets/img/${computerChoice}.jpg" alt="rock"  width="150">
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</strong></p>
+    `;
+  }
+  // Show score
+  score.innerHTML = `
+  <div class="row">
+  <div class="col-12 d-flex align-items-center mb-4">
+   <div class="col-6 text-start">${playerName}</div><div class="scoreBoardL p-2"> ${scoreboard.player}</div>
+  </div>
+  <div class="col-12 d-flex align-items-center">
+   <div class="col-6 text-start">Computer </div><div class="scoreBoardL p-2"> ${scoreboard.computer}</div>
+  </div>
+</div>
+    `;
+
+  modal.style.display = "block";
+}
+
+// Restart game
+function restartGame() {
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
+  gameBoard.style.display = "none";
+  playerNameInput.style.display = "block";
+  playerNameInput.value = "";
+  startGameBtn.style.display = "block";
+}
+
+// Clear modal
+function clearModal(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
   }
 }
 
-header .navbar-brand {
-  height: 150px;
+// Event listeners
+choices.forEach((choice) => choice.addEventListener("click", playGame));
+window.addEventListener("click", clearModal);
+restart.addEventListener("click", restartGame);
+
+// Change Theme
+function setTheme(theme) {
+  document.documentElement.style.setProperty("--primary-color", theme);
+  localStorage.setItem("movie-theme", theme);
 }
+setTheme(localStorage.getItem("movie-theme") || chathams_blue);
